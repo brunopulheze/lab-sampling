69cd332fdbf4a50a2a173b78ebe848d674525660
Bruno Pulheze <brunopulheze@hotmail.com>
2025-10-07 11:35:46 +0200

    Prepare for GitHub Pages

--- src/components/Navbar/SpecialOffers.tsx
+++ src/components/Navbar/SpecialOffers.tsx
@@ -1,7 +1,7 @@
 export function SpecialOffers() {
     return (
         <div className='w-100 bg-warning py-2 text-dark bg-opacity-25 fw-bold text-center' style={{ fontSize: ".9rem" }}>
-            ✨ Please scroll down to answer the questionnaire
+            ✨ Please scroll down to answer the Survey
         </div>
     )
 }
